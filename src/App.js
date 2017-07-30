87db9065671f54407d70335b9a4d82d8514709bd
yangzhiyang <zhiyang_yang@126.com>
2017-07-30 08:58:08 +0800

    完成基本框架

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(...args){
+    super(...args)
+
+    this.state = {
+      newTodo: 'test',
+      todoList: [
+        {id:1,title:'第一个事项'}
+      ]
+    }
+  }
+  render() {
+    let todos = this.state.todoList.map((item,index)=>{
+      return <li>{item.title}</li>
+    })
+    return (
+      <div className="App">
+       <h1>我的待办</h1>
+       <input type="text" value={this.state.newTodo}/>
+       <ol>
+         {todos}
+       </ol>
+      </div>
+    );
+  }
+}
+
+export default App;
