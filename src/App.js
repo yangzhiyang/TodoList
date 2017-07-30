4c21858f115acfb152ad4ea228154c3bf07175cd
yangzhiyang <zhiyang_yang@126.com>
2017-07-30 14:27:05 +0800

    使用componentDidUpdate

--- src/App.js
+++ src/App.js
@@ -34,22 +34,25 @@ class App extends Component {
       </div>
     );
   }
+  componentDidUpdate(){
+    localStore.save('todoList', this.state.todoList)
+  }
   delete(e,todo){
     todo.deleted = true
     this.setState(this.state)
-    localStore.save('todoList', this.state.todoList)
+    
   }
   toggle(e, todo){
     todo.status = todo.status === 'completed' ? '' : 'completed'
     this.setState(this.state)
-    localStore.save('todoList', this.state.todoList)
+    
   }
   changeTitle(e){
     this.setState({
       newTodo: e.target.value,
       todoList: this.state.todoList
     })
-    localStore.save('todoList', this.state.todoList)
+   
   }
   addTodo(e){
     this.state.todoList.push({
@@ -62,7 +65,7 @@ class App extends Component {
       newTodo:'',
       todoList: this.state.todoList
     })
-    localStore.save('todoList', this.state.todoList)
+    
   }
 }
 
