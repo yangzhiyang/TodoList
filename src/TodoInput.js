21350e1150792e2a084a87317202d6c3ac990dd1
yangzhiyang <zhiyang_yang@126.com>
2017-07-30 10:54:52 +0800

    监听回车事件

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -4,10 +4,16 @@ class TodoInput extends Component{
 
     render(){
         return <input type="text" value={this.props.content}
-        onChange={this.changeTitle.bind(this)}/>
+        onChange={this.changeTitle.bind(this)}
+        onKeyPress={this.submit}/>
     }
     changeTitle(e){
         this.props.onChange(e)
+    }
+    submit(e){
+        if(e.key==='Enter'){
+            console.log('按下了回车')
+        }
     }
 }
 
